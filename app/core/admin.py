cca66211c30c4b1b50842f4f34692acfb0198f33
phjae <phjae7@gmail.com>
2025-05-05 10:28:43 +0900

    Feat: setting api docs

--- app/core/admin.py
+++ app/core/admin.py
@@ -24,7 +24,15 @@ class UserAdmin(BaseUserAdmin):
     add_fieldsets = (
         (None, {
             'classes': ('wide',),
-            'fields': ('email', 'password1', 'password2', 'name', 'is_active', 'is_staff', 'is_superuser'),
+            'fields': (
+                'email',
+                'password1',
+                'password2',
+                'name',
+                'is_active',
+                'is_staff',
+                'is_superuser'
+            ),
         }),
     )
 
